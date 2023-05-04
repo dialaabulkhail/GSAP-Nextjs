74f7f14fed4b56f02908f189aa4ff8a400f182b5
dialaabulkhail <diala.sh.98@gmail.com>
2023-05-04 10:17:16 +0000

    initial commit

--- pages/components/Test.js
+++ pages/components/Test.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import { Navbar } from "flowbite-react";
+  
+
 
 const Test = () => {
-  return (
-    <div>
-      hello world
+  return(
+    <div className='w-full  felx justify-center bg-white'>
+      <div className='flex justify-between h-[60px] top-0 sticky place-content-center p-8'>
+        <div className=' flex justify-between gap-2'>
+          <img src="/"/>
+          <div>mega menu</div>
+        </div>
+        <div >
+          <ul className='flex justify-between gap-2'>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+    )
 }
 
 export default Test
