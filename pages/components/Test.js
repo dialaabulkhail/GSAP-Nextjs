5da6511a7a7b50815fa0a02bd95f174f11add826
dialaabulkhail <diala.sh.98@gmail.com>
2023-05-05 14:08:24 +0000

    scroll

--- pages/components/Test.js
+++ pages/components/Test.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import { motion } from 'framer-motion';
-import pets from '../../public/images/logo.svg'
-import Image from 'next/image';
-import  gsap  from 'gsap';
-import {ScrollTrigger} from 'gsap'
-import { useEffect } from 'react';
+
 
 const Test = () => {
  
